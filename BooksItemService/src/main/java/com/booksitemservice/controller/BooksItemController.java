9ff220f4fdb4619d4ef97be5433e6d7709fab062
Soujanya <30731426+SoujanyaPagala1@users.noreply.github.com>
2021-01-21 19:06:58 +0530

    Add files via upload

--- BooksItemService/src/main/java/com/booksitemservice/controller/BooksItemController.java
+++ BooksItemService/src/main/java/com/booksitemservice/controller/BooksItemController.java
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.booksitemservice.entity.BooksItem;
 import com.booksitemservice.model.BooksItemReq;
 import com.booksitemservice.service.BooksItemService;
+import com.google.common.collect.Lists;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * @author Soujanya Pagala
@@ -40,6 +42,7 @@ public class BooksItemController {
 	 * 
 	 * @return
 	 */
+	@HystrixCommand(fallbackMethod = "getFallBackBooksItemAll")
 	@GetMapping("/retriveBooksItem")
 	public ResponseEntity<List<BooksItem>> getAllBooksItems() {
 		LOGGER.info("BooksItemController :: Received Request ....");
@@ -47,6 +50,10 @@ public class BooksItemController {
 		booksItemList = booksItemService.getAllbooksItems();
 		return new ResponseEntity<>(booksItemList, HttpStatus.OK);
 	}
+	
+	 public List<BooksItem> getFallBackBooksItemAll() {
+		 return Lists.newArrayList();
+	 }
 
 	/**
 	 * This method will save the item details into Database
@@ -65,6 +72,7 @@ public class BooksItemController {
 	 * @param id
 	 * @return
 	 */
+	@HystrixCommand(fallbackMethod = "getFallBackBooksItem")
 	@GetMapping("/BooksItemById")
 	public ResponseEntity<BooksItem> getBookItemById(@RequestParam("id") long id) {
 		LOGGER.info("BooksItemController :: Received Request :: getBooksItemById: " + id);
@@ -77,11 +85,20 @@ public class BooksItemController {
 	 * @param ids
 	 * @return
 	 */
+	@HystrixCommand(fallbackMethod = "getFallBackBooksItem")
 	@GetMapping("/BooksItemByIds")
 	public ResponseEntity<Set<BooksItem>> getBookItemByIds(@RequestParam("ids") Set<Long> ids) {
 		LOGGER.info("BooksItemController :: Received Request ::  getBookItemByIds: " + ids.toString());
 		Set<BooksItem> booksItems = booksItemService.getBookItemByBookCode(ids);
 		return new ResponseEntity<>(booksItems, HttpStatus.OK);
-
 	}
+	
+	public BooksItem getFallBackBooksItem(long bookorderId) {
+		  BooksItem data = new BooksItem();
+		  data.setBookId(-1);
+		  data.setBookName("FallBack Method gets called");
+		  data.setBookAuthor("N/A");
+		  return data;
+	  }
+	
 }
